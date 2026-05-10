19477d570ff18516e3b5e6fc81eefbb2328ac9b4
mathiashansen275-arch <mathiashansen275@gmail.com>
2026-05-10 23:54:25 +0200

    Remove clickable hover styling from blocks

--- personal-os-disable-block-edit.js
+++ personal-os-disable-block-edit.js
@@ -1,5 +1,12 @@
 // Personal OS: block manual schedule editing; AI/tools remain able to change state.
 (function(){
+  function ensureStyle(){
+    if(document.getElementById('pos-disable-block-edit-style'))return;
+    const s=document.createElement('style');
+    s.id='pos-disable-block-edit-style';
+    s.textContent='#scheduleView .event{cursor:default!important}#scheduleView .event:hover{transform:none!important;filter:none!important;box-shadow:inherit!important}#scheduleView .event .posAgentDetails{cursor:pointer!important}';
+    document.head.appendChild(s);
+  }
   function protect(e){
     const target=e.target&&e.target.closest&&e.target.closest('#scheduleView .event');
     if(!target)return;
@@ -8,5 +15,6 @@
     e.stopPropagation();
     if(e.stopImmediatePropagation)e.stopImmediatePropagation();
   }
+  ensureStyle();
   ['click','dblclick','mousedown','mouseup','pointerdown','pointerup','touchstart','touchend'].forEach(type=>document.addEventListener(type,protect,true));
 })();
