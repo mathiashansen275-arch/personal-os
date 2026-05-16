2fea907058d056dc60669c5bcd1b4133c00a8c2d
mathiashansen275-arch <mathiashansen275@gmail.com>
2026-05-16 15:40:52 +0200

    Use v1-style shell for v2 entry

--- personal-os-v2-app/src/main.tsx
+++ personal-os-v2-app/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import { AppV1Shell } from './AppV1Shell';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AppV1Shell />
   </React.StrictMode>,
 );
